2cbb4ec0c789601f0b22ddb59adf9337ec67e0dd
suyash-5613 <suyashsahu0405@gmail.com>
2026-05-30 13:18:17 +0530

    final done

---
+++ notification_system_fe/vite.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+var __dirname = path.dirname(fileURLToPath(import.meta.url));
+export default defineConfig({
+    plugins: [react()],
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "src"),
+            "@notification-logger": path.resolve(__dirname, "../logging_middleware/src/index.ts"),
+        },
+    },
+    server: {
+        fs: {
+            allow: [path.resolve(__dirname, "..")],
+        },
+    },
+});
